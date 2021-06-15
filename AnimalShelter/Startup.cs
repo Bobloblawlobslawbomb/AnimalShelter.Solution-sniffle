9a3b9d0542ee5c09361f5dc3596ba0c49b4b9313
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-14 23:33:33 -0700

    :beers:implement Swagger AND versioning (probably)

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -5,6 +5,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
 
 namespace AnimalShelter
 {
@@ -17,15 +22,43 @@ namespace AnimalShelter
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
+      services.AddApiVersioning(config =>
+  {
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    config.ReportApiVersions = true;
+  });
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "AnimalShelter API",
+          Description = "A simple example ASP.NET Core Web API",
+          TermsOfService = new Uri("https://example.com/terms"),
+          Contact = new OpenApiContact
+          {
+            Name = "Giancarlo Vigneri",
+            Email = string.Empty,
+            Url = new Uri("https://github.com/Bobloblawlobslawbomb"),
+          },
+          License = new OpenApiLicense
+          {
+            Name = "Use under LICX",
+            Url = new Uri("https://example.com/license"),
+          }
+        });
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+      });
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
@@ -33,7 +66,12 @@ namespace AnimalShelter
         app.UseDeveloperExceptionPage();
       }
 
-      //app.UseHttpsRedirection();
+      app.UseSwagger();
+
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+      });
 
       app.UseRouting();
 
